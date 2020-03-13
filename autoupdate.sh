66590b2fe2c6cc04a0490f7124a98f6da5dd03c3
postarc <45208608+postarc@users.noreply.github.com>
2020-03-13 19:53:37 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 SCRIPT_NAME="idenaupdate.sh"
+SCRIPT_PATH="idena-script"
+DAEMON_PATH="idena"
 PATH_NAME="https://github.com/idena-network/idena-go.git"
 RPATH_NAME="https://github.com/idena-network/idena-go/releases"
 if [[ "$USER" == "root" ]]; then
-        HOMEFOLDER="/root/idena-scripts"
+        HOMEFOLDER="/root"
  else
-        HOMEFOLDER="/home/$USER/idena-scripts"
+        HOMEFOLDER="/home/$USER"
 fi
 
 CURRENTDIR=$(pwd)
-cd $HOMEFOLDER
+cd $HOMEFOLDER/$SCRIPT_PATH
 
 if [[ -z $(sudo -u root crontab -l | grep 'idenaupdate.sh') ]]; then
         sudo -u root crontab -l > cron
-        echo -e "0 */1 * * * $HOMEFOLDER/idenaupdate.sh >/dev/null 2>&1" >> cron
+        echo -e "0 */1 * * * $HOMEFOLDER/$SCRIPT_PATH/idenaupdate.sh >/dev/null 2>&1" >> cron
         sudo -u root crontab cron
         rm cron
 fi
@@ -26,34 +28,32 @@ echo -e "GITPATH=$PATH_NAME" >> $SCRIPT_NAME
 echo -e "RELEASES_PATH=$RPATH_NAME" >> $SCRIPT_NAME
 echo 'FILE_NAME="idena-node-linux-"' >> $SCRIPT_NAME
 echo 'CURRENTDIR=$(pwd)' >> $SCRIPT_NAME
-echo -e "cd $HOMEFOLDER" >> $SCRIPT_NAME
+echo -e "cd $HOMEFOLDER/$SCRIPT_PATH" >> $SCRIPT_NAME
 echo 'if [ -d idena-go ]; then' >> $SCRIPT_NAME
 echo '  cd idena-go' >> $SCRIPT_NAME
 echo '  git fetch' >> $SCRIPT_NAME
 echo '  else' >> $SCRIPT_NAME
 echo '  git clone $GITPATH' >> $SCRIPT_NAME
-#echo '  cd idena-go' >> $SCRIPT_NAME
+echo '  cd idena-go' >> $SCRIPT_NAME
 echo 'fi' >> $SCRIPT_NAME
-echo -e "chown -R $USER:$USER $HOMEFOLDER/idena-go" >> $SCRIPT_NAME
+
+#echo -e "chown -R $USER:$USER $HOMEFOLDER/idena-go" >> $SCRIPT_NAME
 echo 'LATEST_TAG=$(git tag --sort=-creatordate | head -1)' >> $SCRIPT_NAME
 echo 'cd ..' >> $SCRIPT_NAME
+echo 'LATEST_TAG=${LATEST_TAG//v/}' >> $SCRIPT_NAME
 echo -n 'if [[ -z $' >> $SCRIPT_NAME
-echo -n -e "($HOMEFOLDER/nknd -v | grep " >> $SCRIPT_NAME
+echo -n -e "($HOMEFOLDER/$DAEMON_PATH/idena-node -v | grep" >> $SCRIPT_NAME
 echo '$LATEST_TAG) ]]; then' >> $SCRIPT_NAME
-
-echo '  FILE_NAME+=${LATEST_TAG//v/}' >> $SCRIPT_NAME
-echo '  if [ -f $DIR_NAME ]; then rm $DIR_NAME; fi' >> $SCRIPT_NAME
-echo '  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME"' >> $SCRIPT_NAME
-echo '  mv $DIR_NAME idena-node' >> $SCRIPT_NAME
-#echo '  chmod +x $DIR_NAME/nkn*' >> $SCRIPT_NAME
-echo '  cp -r $DIR_NAME/* .' >> $SCRIPT_NAME
-
-echo '  rm -rf $DIR_NAME' >> $SCRIPT_NAME
-echo '  rm $DIR_NAME.zip' >> $SCRIPT_NAME
+echo '  FILE_NAME+=$LATEST_TAG' >> $SCRIPT_NAME
+echo '  if [ -f $DIR_NAME ]; then rm $FILE_NAME; fi' >> $SCRIPT_NAME
+echo '  wget "$RELEASES_PATH/$LATEST_TAG/$FILE_NAME"' >> $SCRIPT_NAME
+echo '  chmod +x $FILE_NAME' >> $SCRIPT_NAME
+echo '  mv $FILE_NAME $HOMEFOLDER/$DAEMON_PATH/idena-node' >> $SCRIPT_NAME
 echo 'fi' >> $SCRIPT_NAME
-echo 'LATEST_TAG=$( ps -e | grep idenad | awk '{print $1}' )' >> $SCRIPT_NAME
+
+#echo 'LATEST_TAG=$( ps -e | grep idena-node | awk '{print $1}' )' >> $SCRIPT_NAME
 #echo 'if [ -z $LATEST_TAG ]; then systemctl start nkn.service; fi' >> $SCRIPT_NAME
-echo 'cd $CURRENTDIR' >> $SCRIPT_NAME
+#echo 'cd $CURRENTDIR' >> $SCRIPT_NAME
 
 chmod +x $SCRIPT_NAME
 cd $CURRENTDIR
