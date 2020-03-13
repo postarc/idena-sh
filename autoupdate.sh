7d52b7c9d6a4f113d4e4dbfc073c892c75fe11f7
postarc <45208608+postarc@users.noreply.github.com>
2020-03-13 19:08:53 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -40,21 +40,21 @@ echo 'cd ..' >> $SCRIPT_NAME
 echo -n 'if [[ -z $' >> $SCRIPT_NAME
 echo -n -e "($HOMEFOLDER/nknd -v | grep " >> $SCRIPT_NAME
 echo '$LATEST_TAG) ]]; then' >> $SCRIPT_NAME
-#echo -e "  sudo -u $USER systemctl stop nkn.service" >> $SCRIPT_NAME
+
+echo '  DIR_NAME+=${LATEST_TAG//v/}'
 echo '  if [ -f $DIR_NAME.zip ]; then rm $DIR_NAME.zip; fi' >> $SCRIPT_NAME
 echo '  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"' >> $SCRIPT_NAME
 echo '  unzip "$DIR_NAME.zip" >/dev/null 2>&1' >> $SCRIPT_NAME
-echo '  chmod +x $DIR_NAME/nkn*' >> $SCRIPT_NAME
+#echo '  chmod +x $DIR_NAME/nkn*' >> $SCRIPT_NAME
 echo '  cp -r $DIR_NAME/* .' >> $SCRIPT_NAME
-echo '  rm Log/*' >> $SCRIPT_NAME
-echo -e "  sudo -u $USER systemctl start nkn.service" >> $SCRIPT_NAME
+
 echo '  rm -rf $DIR_NAME' >> $SCRIPT_NAME
 echo '  rm $DIR_NAME.zip' >> $SCRIPT_NAME
 echo 'fi' >> $SCRIPT_NAME
-echo 'LATEST_TAG=$( ps -e | grep nknd | awk '{print $1}' )' >> $SCRIPT_NAME
-echo 'if [ -z $LATEST_TAG ]; then systemctl start nkn.service; fi' >> $SCRIPT_NAME
+echo 'LATEST_TAG=$( ps -e | grep idenad | awk '{print $1}' )' >> $SCRIPT_NAME
+#echo 'if [ -z $LATEST_TAG ]; then systemctl start nkn.service; fi' >> $SCRIPT_NAME
 echo 'cd $CURRENTDIR' >> $SCRIPT_NAME
 
 chmod +x $SCRIPT_NAME
 cd $CURRENTDIR
-rm -rf nkn
+
