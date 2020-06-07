e4ab58b16bd6eb9402d905e75eb04555c1aefb0f
postarc <45208608+postarc@users.noreply.github.com>
2020-06-07 20:34:47 +0300

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DAEMON_FILE='idena-node'
+NODE_DIR='idena'
+SCRIPT_DIR='idena-scripts'
+
+#color
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m"
+PURPLE="\033[0;35m"
+RED='\033[0;31m'
+GREEN="\033[0;32m"
+NC='\033[0m'
+MAG='\e[1;35m'
+
+echo -e "${YELLOW}Stop & remove service...${NC}"
+sudo systemctl stop idena.serivce
+sudo systemctl disable idena.service
+echo -e "${YELLOW}Remove directory...${NC}"
+if [ -d $NODE_DIR ]; then rm -rf $NODE_DIR; fi
+if [ -d $SCRIPT_DIR ]; then rm -rf $SCRIPT_DIR; fi
+echo -e "${YELLOW}Cleaning crontab...${NC}"
+sudo crontab -l > cron
+sed '/$SCRIPT_NAME/d' cron
+
+#echo -e "0 */1 * * * $HOMEFOLDER/$SCRIPT_PATH/$SCRIPT_NAME >/dev/null 2>&1" >> cron
