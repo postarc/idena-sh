9521c2ef9d9b449e7b3fd28092effda4d07778cc
postarc <45208608+postarc@users.noreply.github.com>
2020-08-30 19:23:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ sudo apt install -y git jq curl
 echo -e "{\n  \"P2P\": {\n   \"ListenAddr\": \": $P2P_PORT\" },\n" > $HOMEFOLDER/$NODE_DIR/config.json
 #, "BootstrapNodes": [], "NoDiscovery": false },
 echo -e "  \"RPC\": {\n   \"HTTPHost\": \"localhost\",\n   \"HTTPPort\": $RPCPORT },\n" >> $HOMEFOLDER/$NODE_DIR/config.json
-echo -e "  \"Ipfsconf\": {\n   \"Profile\": \"server\",\n   \"IpfsPort\": $IPFSPORT, },\n" >> $HOMEFOLDER/$NODE_DIR/config.json
+echo -e "  \"Ipfsconf\": {\n   \"Profile\": \"server\",\n   \"IpfsPort\": $IPFSPORT },\n" >> $HOMEFOLDER/$NODE_DIR/config.json
 echo -e "  \"Sync\": {\n   \"FastSync\": true }\n }" >> $HOMEFOLDER/$NODE_DIR/config.json
 
 echo -e "${GREEN}Creating idena service...${NC}"
