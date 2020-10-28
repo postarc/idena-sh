c481c49a5fb53b417b18c998945dc81000ceae1c
postarc <45208608+postarc@users.noreply.github.com>
2020-10-28 18:52:43 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -39,9 +39,8 @@ echo 'fi' >> $SCRIPT_NAME
 echo 'LATEST_TAG=$(git tag --sort=-creatordate | head -1)' >> $SCRIPT_NAME
 echo 'cd ..' >> $SCRIPT_NAME
 echo 'LATEST_TAG=${LATEST_TAG//v/}' >> $SCRIPT_NAME
-echo -n 'if [[ -z $' >> $SCRIPT_NAME
-echo -n -e "($HOMEFOLDER/$DAEMON_PATH/$DAEMON_FILE -v | grep " >> $SCRIPT_NAME
-echo '$LATEST_TAG) ]]; then' >> $SCRIPT_NAME
+echo -e "DAEMON_VERSION=$($HOMEFOLDER/$DAEMON_PATH/$DAEMON_FILE -v | awk '{print $3}')" >> $SCRIPT_NAME
+echo -n 'if [ $DAEMON_VERSION = $LATEST_TAG ]; then' >> $SCRIPT_NAME
 echo '  FILE_NAME+=$LATEST_TAG' >> $SCRIPT_NAME
 echo '  if [ -f $FILE_NAME ]; then rm $FILE_NAME; fi' >> $SCRIPT_NAME
 echo '  wget "$RELEASES_PATH/v$LATEST_TAG/$FILE_NAME"' >> $SCRIPT_NAME
